1cca42c50eb1ce881ab8608f3ca38d9fc9f65fa6
unknown <Jorgemathez14@gmail.com>
2024-06-18 20:11:06 -0300

    Concatenacion

---
+++ Leccion3/ConcatenacionDeCadenas.js
@@ -0,0 +1,15 @@
+var nombre = "Jorge";
+var apellido = "Mathez";
+var nombreCompleto = nombre + "" + apellido;
+console.log(nombreCompleto);
+var nombreCompleto2 = "Jorge" +" "+ "Mathez";
+console.log(nombreCompleto2);
+var juntos = nombre + 219;
+console.log(juntos);
+juntos = nombre + 78 + 17;
+console.log(juntos);
+nombre += apellido;
+console.log(nombre);
+
+
+
