1630a23fc262272c3c0c698ed2b9c6d3ef70bf07
unknown <Jorgemathez14@gmail.com>
2024-06-27 16:40:18 -0300

    finalizado JS

---
+++ Clase10/Clase10.js
@@ -0,0 +1,69 @@
+//ejercicio 1 : calcular la estacion del año
+/*
+let mes = 4;
+let estacion; // undefined
+if (mes === 1 || mes === 2 || mes === 12) {
+    estacion = "verano";
+}
+else if (mes === 3 || mes === 4 || mes === 5) {
+    estacion = "otoño";
+}
+else if (mes === 6 || mes === 7 || mes === 8) {
+    estacion = "invierno";
+}
+else if (mes === 9 || mes === 10 || mes === 11) {
+    estacion = "primavera";
+}
+else {
+    estacion = "valor incorrecto";
+}
+console.log("El valor corresponde a la estacion de :" + estacion);*/
+
+//ejercicio 2 : hora del dia
+/* 
+de 6 a 11 nos saluda : good mornign
+de 12 a 16 nos saluda: good afternoon
+de 17 a19 nos saluda : good evening
+de 20 a 23 nos saluda : good night
+trabajemos con 24 horas
+*/
+
+/*
+let horaDia = 9;
+let mensaje;
+if (horaDia >= 6 && horaDia <= 11) {
+    mensaje = "Good morning";
+}
+else if (horaDia >= 12 && horaDia <= 16) {
+    mensaje = "Good afternoon";
+}
+else if (horaDia >= 17 && horaDia <= 19) {
+    mensaje = "Good evening";
+}
+else if (horaDia >= 20 && horaDia <= 23) {
+    mensaje = "Good night";
+}
+else {
+    mensaje = "Valor incorrecto";
+}
+console.log(mensaje);*/
+
+//estructura switch (la sintaxis es igual a java)
+
+switch(mes){
+    case 1:case 2:case 12:
+        estacion = "verano";
+        break;
+    case 3:case 4:case 5:
+        estacion = "otoño";
+        break;
+    case 6:case 7:case 8:
+        estacion = "invierno";
+        break;
+    case 9:case 10:case 11:
+        estacion = "primavera";
+        break;
+    default:
+        estacion = "valor incorrecto";
+}
+console.log("Bienvenido a la estacion de:" + estacion);
---
+++ Clase11/Clase11.js
@@ -0,0 +1,143 @@
+var nombre = "Jorge";
+nombre = "Mathez";
+console.log(nombre);
+
+function saludar(){
+    var nombre3 = "Colifaa";
+    console.log (nombre);
+}
+
+//console.log(nombre3) // aqui no lee el dato en la funcion
+
+if(true){
+    var edad = 34;
+    console.log(edad);
+
+}
+console.log (edad);
+
+function saludar2(){
+    let nombre2 = "Noelia";
+    console.log(nombre2);
+}
+console.log (nombre2);
+
+if(true){
+    let edad2 = 33;
+    console.log (edad2)
+}
+
+console.log (edad2);
+
+// const se utiliza para valores constantes que no pueden ser reasignadas
+
+const fechaNacimiento = 2006;
+console.log (fechaNacimiento);
+
+//fechaNacimiento = 2003;
+//console.log(fechaNacimiento); // solo se ejecuta en la consola anterior
+
+//evitar repetir tu codigo
+//dry dont' repat yourself
+
+//let days =['Lunes','Martes','Miercoles' ,'Jueves', 'Vienres', 'Sabado', 'Domingo'];
+
+let days = 1;
+switch(days){
+    case 1:
+        console.log("Hoy es lunes" );
+        break;
+    case 2:
+        console.log("Hoy es martes" );
+        break;
+    case 3:
+        console.log("Hoy es miercoles" );
+        break;
+    case 4:
+        console.log("Hoy es jueves" );
+        break;
+    case 5:
+        console.log("Hoy es viernes" );
+        break;
+    case 6:
+        console.log("Hoy es sabado");
+        break;
+    case 7:
+        console.log("Hoy es domingo" );
+        break;
+    default:
+        break;
+}
+
+
+
+// esta es la opcion mejorada
+let days2 =['Lunes','Martes','Miercoles' ,'Jueves', 'Vienres', 'Sabado', 'Domingo'];
+function getday(n){
+    if(n<1 || n >7){
+        throw new error ('out of range');
+    }
+    return days2[n-1];
+}
+
+console.log(getday(5));
+
+
+// hacer un ejercicio similar al que esta hecho, pero ahora con los
+//meses del año , debes hacerlo con la estructura switch y luego
+//con la funcion en la opcion mejorada
+
+let month = 11;
+switch(month){
+    case 1:
+        console.log("Es enero");
+        break;
+    case 2:
+        console.log("Es febrero");
+        break;
+    case 3:
+        console.log("Es marzo");
+        break;
+    case 4 :
+        console.log("Es abril");
+        break;
+    case 5:
+        console.log("Es mayo");
+        break;
+    case 6:
+        console.log("Es junio");
+        break;
+    case 7:
+        console.log("Es julio");
+        break;
+    case 8:
+        console.log("Es agosto");
+        break;
+    case 9:
+        console.log("Es septiembre");
+        break;
+    case 10:
+        console.log("Es octubre");
+        break;
+    case 11:
+        console.log("Es noviembre");
+        break;
+    case 12:
+        console.log("Es diciembre");
+        break;
+    default:
+        console.log ("Error en el ingreso del mes del año");
+        break;
+}
+
+// esta es la opcion mejorada
+
+let Month2 = ['Enero','Febrero','Marzo' ,'Abril', 'Mayo', 'Junio', 'Julio' , 'Agosto','Septiembre','Octubre', 'Noviembre','Diciembre'];
+function getMonth(n){
+    if(n < 1 || n > 12){
+        throw new error("out of range");
+    }
+    return Month2[n-1];
+}
+
+console.log(getMonth(1));
